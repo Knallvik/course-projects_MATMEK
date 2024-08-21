5dbdfc142443b5bfb292bd2fda1ab77394420ac3
Knallvik <dkalvik@gmail.com>
2024-08-21 13:57:25 +0200

    completed mesh_functions_py

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -1,11 +1,18 @@
-import numpy as np#
+import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    y = np.empty_like(t)
+    for i, t_ in enumerate(t):
+        y[i] = f(t_)
+    return y
 
 def func(t):
-    pass
+    if t>=0 and t<=3:
+        return np.exp(-t)
+    elif t>3 and t<=4:
+        return np.exp(-3*t)
+    else: print('outside of range')
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
