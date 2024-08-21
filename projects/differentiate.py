48e336fbc19b24331ad02dab5d4e4fece0c064ad
Knallvik <dkalvik@gmail.com>
2024-08-21 15:27:52 +0200

    Completed differentiate problem

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,21 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    du = np.zeros_like(u)
+    du[0] = (u[1]-u[0])/dt
+    du[-1] = (u[-1]-u[-2])/dt
+    for i in range(1,u.size-1):
+        du[i] = (u[i+1]-u[i-1])/2/dt
+    return du
 
 def differentiate_vector(u, dt):
-    pass
+    u_p = u[2:]
+    u_m = u[:-2]
+    duv = np.zeros_like(u)
+    duv[1:-1] = 1/2/dt*(u_p-u_m)
+    duv[0] = (u[1]-u[0])/dt
+    duv[-1] = (u[-1]-u[-2])/dt
+    return duv
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    y = np.empty_like(t)
+    y = np.empty_like(t, dtype = float)
     for i, t_ in enumerate(t):
         y[i] = f(t_)
     return y
@@ -17,5 +17,7 @@ def func(t):
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
     f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
+    print(f)
     fun = mesh_function(func, t)
+    print(fun)
     assert np.allclose(fun, f)
